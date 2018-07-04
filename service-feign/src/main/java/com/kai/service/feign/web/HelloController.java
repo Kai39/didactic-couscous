581169b4c9f70b6bb21178ad7d6c712c2d9600b4
kaijiadong <kaijia.dong@rong-data.com>
2018-07-04 10:45:36 +0800

    第三章 1.子项目Feign建立

---
+++ service-feign/src/main/java/com/kai/service/feign/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.kai.service.feign.web;
+
+import com.kai.service.feign.service.SchedualEurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    SchedualEurekaClient schedualEurekaClient;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualEurekaClient.sayHiFromClientOne(name);
+    }
+
+}
